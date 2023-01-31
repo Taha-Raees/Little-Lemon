6bb807d9455d93915a22c1c93deb0a0944bae132
Taha-Raees <raeesmuhammadtaha@gmail.com>
2023-01-31 23:41:37 +0100

    first comt

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
     <div className="app">
       <ScrollToTop/>
       <Navbar />
-      <Routes>
+      <Routes basename="/Little-Lemon">
         <Route path="/" element={<Home />}></Route>
         <Route path="/menu" element={<Menu />}></Route>
         <Route path="/reservation" element={<Reservation />}></Route>
